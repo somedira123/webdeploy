aec76db84bb428bd8c290bf29fca5e6095cfd9a1
somedira123 <131841679+somedira123@users.noreply.github.com>
2023-07-01 21:08:30 +0700

    connection.inc.php

--- ecom/connection.inc.php
+++ ecom/connection.inc.php
@@ -10,7 +10,7 @@ $con=mysqli_connect("database-1.ccbzs0t7ckjf.us-east-1.rds.amazonaws.com","ecom1
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/Electronix-Website/ecom/');
 
 //Edit the IP address (127.0.0.1) with the Public IP address of your EC2 instance
-define('SITE_PATH','http://127.0.0.1/Electronix-Website/ecom/');
+define('SITE_PATH','http://54.197.37.101/Electronix-Website/ecom/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
